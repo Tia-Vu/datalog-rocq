1326f7051fec758abc0f66db3726ec91beaf0e4d
tnv5 <tnv5@cornell.edu>
2025-12-16 23:54:33 -0800

    added soundness proof

--- src/Examples/Layouts/GridLayout.v
+++ src/Examples/Layouts/GridLayout.v
@@ -1,15 +1,28 @@
 From Stdlib Require Import List Bool Lia.
 From Datalog Require Import Datalog.
-From DatalogRocq Require Import Dataflow GridGraph DependencyGenerator.
+From DatalogRocq Require Import Dataflow GridGraph.
+From coqutil Require Import Map.Interface.
 Import ListNotations.
 
-Module GridLayout (Params : DatalogParams).
-  Import Params.
-  Parameter (dims : list nat).
+Section GridLayout.
+  Context {rel var fn aggregator T : Type}.
+  Context `{sig : signature fn aggregator T} `{query_sig : query_signature rel}.
+  Context {context : map.map var T} {context_ok : map.ok context}.
+  Context {var_eqb : var -> var -> bool} {var_eqb_spec :  forall x0 y0 : var, BoolSpec (x0 = y0) (x0 <> y0) (var_eqb x0 y0)}.
+  Context {rel_eqb : rel -> rel -> bool} {rel_eqb_spec : forall x0 y0 : rel, BoolSpec (x0 = y0) (x0 <> y0) (rel_eqb x0 y0)}.
+  Context {fn_eqb : fn -> fn -> bool} {fn_eqb_spec : forall x0 y0 : fn, BoolSpec (x0 = y0) (x0 <> y0) (fn_eqb x0 y0)}.
+  Context {aggregator_eqb : aggregator -> aggregator -> bool}
+          {aggregator_eqb_spec : forall x0 y0 : aggregator, BoolSpec (x0 = y0) (x0 <> y0) (aggregator_eqb x0 y0)}.
+
   Definition rule := Datalog.rule rel var fn aggregator.
+
+  Context {rule_eqb : rule -> rule -> bool}.
+  Context {rule_eqb_spec : forall r1 r2 : rule,
+                            BoolSpec (r1 = r2) (r1 <> r2) (rule_eqb r1 r2)}.
+
+  Parameter (dims : list nat).
   Parameter (program : list rule).
   Parameter (indexed_layout : list (Node * list nat)).
-  Module dependency_generator := DependencyGenerator(Params).
 
   Definition grid_graph : Dataflow.Graph := GridGraph dims.
 
@@ -29,11 +42,11 @@ Module GridLayout (Params : DatalogParams).
     else [].
 
   Definition rule_in_layout (r : rule) : bool :=
-    existsb (fun n => existsb (dependency_generator.rule_eqb r) (layout n))
+    existsb (fun n => existsb (rule_eqb r) (layout n))
             (all_nodes_h dims).
 
   Definition node_rules_ok (n : Node) : bool :=
-    forallb (fun r => existsb (dependency_generator.rule_eqb r) program)
+    forallb (fun r => existsb (rule_eqb r) program)
             (layout n).
 
   Definition check_layout : bool :=
@@ -68,7 +81,7 @@ Proof.
       rewrite existsb_exists in H_r_in_layout.
       destruct H_r_in_layout as [r H_r_eq].
       exists n. destruct H_r_eq as [Hin H_r_eq]. 
-      destruct (dependency_generator.rule_eqb_spec x r).
+      destruct (rule_eqb_spec x r).
       + subst. split; auto. apply all_nodes_correct. apply H_n_in_nodes.
       + discriminate H_r_eq.
     - intros.
@@ -87,9 +100,55 @@ Proof.
         rewrite existsb_exists in H_nodes_ok.
         destruct H_nodes_ok as [r' H_r'_in_program].
         destruct H_r'_in_program as [Hin H_r_eq].
-        destruct (dependency_generator.rule_eqb_spec r r').
+        destruct (rule_eqb_spec r r').
         * subst. auto.
         * discriminate H_r_eq.
+Qed.
+
+Definition always_forward_table (n : Node) : rel * (list T) -> list Node :=
+  fun f => filter (GridGraph.is_neighbor dims n) (all_nodes_h dims).
+
+Definition no_inputs (n : Node) (f : rel * (list T)) : Prop := False.
+
+Definition all_outputs (n : Node) (f : rel * (list T)) : Prop := True.
+  
+Definition DataflowNetwork : Dataflow.DataflowNetwork :=
+  {|
+    Dataflow.graph := grid_graph;
+    Dataflow.layout := layout;
+    Dataflow.forward := always_forward_table;
+    Dataflow.input := no_inputs;
+    Dataflow.output := all_outputs
+  |}.
+
+Lemma good_network :
+  check_layout = true ->
+  Dataflow.good_network DataflowNetwork program.
+Proof.
+  intros Hcheck.
+  unfold DataflowNetwork. unfold good_network.
+  split.
+  - apply GridGraph.good_graph.
+  - split. 
+    + apply good_layout. assumption.
+    + split.
+      * simpl. unfold good_forwarding. intros. unfold always_forward_table in H.
+        apply filter_In in H.
+        destruct H as [Hneighbor Hin].
+        apply GridGraph.is_neighbor_correct in Hin.
+        split; try inversion Hin; auto.
+      * simpl. unfold good_input. intros. inversion H.
+Qed.
+
+Theorem soundness :
+  forall f : rel * list T,
+    check_layout = true ->
+    network_prog_impl_fact DataflowNetwork f ->
+    prog_impl_fact program f.
+Proof.
+  intros.
+  apply (Dataflow.soundness DataflowNetwork program); auto.
+  apply good_network; auto.
 Qed.
 
 End GridLayout.
